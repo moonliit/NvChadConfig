00de445616ea88a1f8d5fc3d4ced79c6550d5801
moonliit <d4rkmoon0503@gmail.com>
2025-09-19 16:51:18 -0500

    test

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,12 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+
+-- Ctrl+Num to switch to a buffer
+for i = 1, 9, 1 do
+  map("n", string.format("<C-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end, { desc = string.format("Switch to %s-th buffer", i) })
+end
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
